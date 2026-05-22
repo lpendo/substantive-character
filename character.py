8afc7281232c02d559355d6426165d293600ead3
Luke Pendo <lukepend@buffalo.edu>
2026-05-21 23:45:21 -0400

    Initial work on Character sheet datatype.

--- character.py
+++ character.py
@@ -7,18 +7,40 @@ class BaseQuality:
     obj_data: str
 
 @dataclass
-class QualityWithDot(BaseQuality):
+class DotQuality(BaseQuality):
     dots: int = 0
 
 
 
+# class Character:
+#     def __init__(
+#             self, 
+#             name: str,
+#             chronicle: str
+#     ):
+#         pass
+
+@dataclass
 class Character:
-    def __init__(
-            self, 
-            name: str,
-            chronicle: str
-    ):
-        pass
+    name: str
+    chronicle: str
+
+    inherent_splat: BaseQuality
+    social_splat: BaseQuality
+
+    soul_trait_1: BaseQuality
+    soul_trait_2: BaseQuality
+
+    big_goal: BaseQuality
+    small_goal: BaseQuality
+
+    health: int
+    willpower: int
+    integrity: BaseQuality
+
+    
+    
+    
     
     
     
